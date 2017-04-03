7819f6de7bee58cc058999bf98bd866221576e16
agath <agath@172.17.72.45>
2017-04-03 12:27:29 +0200

    Manque la taille du JtextArea

--- default/view/Evaluation.java
+++ default/view/Evaluation.java
@@ -8,17 +8,18 @@ public class Evaluation extends JPanel{
 	private JPanel evaluation;
 	// contient les boutons evaluerTravailEleve et validerChoixEvaluation
 	private JPanel note;
-	// contient les checkboxs
-	private JPanel checkBox;
+	// contient les boutonradio
+	private JPanel radioBouton;
 	// contient le bouton jouerTentative
 	private JPanel jouer;
 	// contient les commentaires du prof
 	private JPanel commentaires;
 	
-	// checkbox qui permet de choisir entre "Acquis", "En cours d'acquisition" et "Non acquis"
-	private JCheckBox acquis = new JCheckBox("Acquis");
-	private JCheckBox enCours = new JCheckBox("En cours d'acquisition");
-	private JCheckBox nonAcquis = new JCheckBox("Non acquis");
+	// bouton radio qui permet de choisir entre "Acquis", "En cours d'acquisition" et "Non acquis"
+	private JRadioButton acquis = new JRadioButton("Acquis");
+	private JRadioButton enCours = new JRadioButton("En cours d'acquisition");
+	private JRadioButton nonAcquis = new JRadioButton("Non acquis");
+	private ButtonGroup bg = new ButtonGroup();
 	
 	// bouton qui permet de valider l'evaluation
 	private JButton validerChoixEvaluation;
@@ -35,7 +36,7 @@ public class Evaluation extends JPanel{
 		//Instanciation des JPanels
 		evaluation = new JPanel(new BorderLayout());
 		note = new JPanel(new GridLayout(3,1));
-		checkBox = new JPanel(new GridLayout(1,3));
+		radioBouton = new JPanel(new GridLayout(1,3));
 		jouer = new JPanel();
 		commentaires = new JPanel();
 		
@@ -46,18 +47,23 @@ public class Evaluation extends JPanel{
 		//cela se fait via la méthode "String monJTextArea.getText()"
 		com = new JTextArea("Votre commentaire ..");
 		com.setEditable(true);
+		com.setLineWrap(true);
+		com.setSize(200,50);
 		
 		//Instanciation des boutons
 		validerChoixEvaluation = new JButton("Valider l'evaluation");
 		jouerTentative = new JButton("Jouer");
 		
 		//Ajout des boutons, textArea, checkbox aux panels
-		checkBox.add(acquis);
-		checkBox.add(enCours);
-		checkBox.add(nonAcquis);
+		bg.add(acquis);
+		bg.add(enCours);
+		bg.add(nonAcquis);
+		radioBouton.add(acquis);
+		radioBouton.add(enCours);
+		radioBouton.add(nonAcquis);
 		
 		note.add(eval);
-		note.add(checkBox);
+		note.add(radioBouton);
 		note.add(validerChoixEvaluation);
 		
 		jouer.add(jouerTentative);
