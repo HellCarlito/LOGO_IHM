1edc0ee1a33d01fdfa48db564497b67bcb91afaf
agath <agath@172.17.72.45>
2017-04-03 11:04:15 +0200

    DessinExercice

--- default/view/DessinExercice.java
+++ default/view/DessinExercice.java
@@ -1,5 +1,26 @@
 package view;
+import javax.swing.*;
+import java.awt.*;
 
-public class DessinExercice {
-
+public class DessinExercice extends JPanel{
+	
+	// il s'agit de la zone des dessins
+	private JPanel dessins;
+	// il s'agit du dessin de l'enfant
+	private JPanel dessinEnfant;
+	//il s'agit du dessin du prof
+	private JPanel dessinProf;
+	
+	public DessinExercice()
+	{
+		//Instanciation des panels
+		dessins = new JPanel();
+		dessinEnfant = new JPanel();
+		dessinProf = new JPanel();
+		
+		//Association des sous-panels au panel général
+		dessins = new JPanel(new GridLayout(1,2));
+		dessins.add(dessinEnfant);
+		dessins.add(dessinProf);
+	}
 }
