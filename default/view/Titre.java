6291dee0cd76145a50041ff3454f6bf18aba95e5
Pierre-Joseph <Pierre-Joseph@172.17.72.209>
2017-04-03 12:09:54 +0200

    Titre modifié

--- default/view/Titre.java
+++ default/view/Titre.java
@@ -3,16 +3,26 @@ import java.awt.*;
 import javax.swing.*;
 import java.util.*;
 
-public class Titre extends JTextArea {
+public class Titre extends JPanel
+{
 	private String text;
+	private JButton retourMenuPrincipal;
+	private Icon quitterIcon;
 	
 	public Titre(String monText){
-		super();
-		initialize(monText);
+		JLabel monTitre = new JLabel(monText);
+		quitterIcon = new ImageIcon(this.getClass().getResource("/pictures/quitter.png"));
+		retourMenuPrincipal = new JButton (quitterIcon);
+		this.setLayout(new GridLayout(1,2));
+		this.add(monTitre);
+		this.add(retourMenuPrincipal);
 	}
 	
-	private void initialize(String myText) {
-    	this.append(myText); 
+	public static void main(String[] args) {
+        JFrame test = new JFrame();
+        test.setContentPane(new Titre("bonjour"));
+        test.setVisible(true);
+        test.pack();
     }
 
 }
