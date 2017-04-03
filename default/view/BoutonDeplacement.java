da69587fb6b007fdcb00fd90819bc72e9aac3a79
Chloé <Chlo@172.17.68.100>
2017-04-03 12:00:23 +0200

    changement image + bouton déplacement ok

--- default/view/BoutonDeplacement.java
+++ default/view/BoutonDeplacement.java
@@ -7,26 +7,31 @@ public class BoutonDeplacement extends JPanel
 {
 	private JButton ecrire,nePasEcrire,tourner,avancer;
 	private Icon ecrireIcon,nePasEcrireIcon,tournerIcon,avancerIcon;
-	private JPanel monPanel;
 	
 	public BoutonDeplacement()
 	{
-		ecrireIcon = new ImageIcon(this.getClass().getResource("/ecrire.png"));
+		ecrireIcon = new ImageIcon(this.getClass().getResource("/pictures/ecrire.png"));
 		ecrire = new JButton (ecrireIcon);
-		nePasEcrireIcon = new ImageIcon(this.getClass().getResource("/nepasecrire.png"));
+		nePasEcrireIcon = new ImageIcon(this.getClass().getResource("/pictures/nepasecrire.png"));
 		nePasEcrire = new JButton (nePasEcrireIcon);
-		tournerIcon = new ImageIcon(this.getClass().getResource("/tourner.png"));
+		tournerIcon = new ImageIcon(this.getClass().getResource("/pictures/tourner.png"));
 		tourner = new JButton (tournerIcon);
-		avancerIcon = new ImageIcon(this.getClass().getResource("/avancer.png"));
+		avancerIcon = new ImageIcon(this.getClass().getResource("/pictures/avancer.png"));
 		avancer = new JButton (avancerIcon);
-		//monPanel= new JPanel("My inventory");
-		//this.set
+		this.setLayout(new GridLayout (1,0));
+		this.add(ecrire);
+		this.add(nePasEcrire);
+		this.add(tourner);
+		this.add(avancer);
+		//this.add(new JLabel("test"));
 	}
 	
-	public JPanel getPanelBoutonDeplacement()
-	{
-		return monPanel;
-	}
+	public static void main(String[] args) {
+        JFrame test = new JFrame();
+        test.setContentPane(new BoutonDeplacement());
+        test.setVisible(true);
+        test.pack();
+    }
 	
 	public JButton getEcrire()
 	{
