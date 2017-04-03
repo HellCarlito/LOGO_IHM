d2660a689aa2fda41beca365bf0647ff68cc607e
Chloé <Chlo@172.17.68.100>
2017-04-03 12:25:30 +0200

    bouton couleur ok

--- default/view/BoutonCouleur.java
+++ default/view/BoutonCouleur.java
@@ -1,5 +1,69 @@
 package view;
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
 
-public class BoutonCouleur {
-
+public class BoutonCouleur extends JPanel
+{
+	private JButton black, red, green, blue, yellow, magenta;
+	
+	public BoutonCouleur()
+	{
+		black = new JButton (" ");
+        black.setBackground(Color.black);
+        red = new JButton (" ");
+        red.setBackground(Color.red);
+        green = new JButton (" ");
+        green.setBackground(Color.green);
+        blue = new JButton (" ");
+        blue.setBackground(Color.blue);
+        yellow = new JButton (" ");
+        yellow.setBackground(Color.yellow);
+        magenta = new JButton (" ");
+        magenta.setBackground(Color.magenta);
+		this.setLayout(new GridLayout (0,2));
+		this.add(yellow);
+		this.add(red);
+		this.add(green);
+		this.add(blue);
+		this.add(black);
+		this.add(magenta);
+	}
+	
+	public static void main(String[] args) {
+        JFrame test = new JFrame();
+        test.setContentPane(new BoutonCouleur());
+        test.setVisible(true);
+        test.pack();
+    }
+	
+	public JButton getBlack()
+	{
+		return black;
+	}
+	
+	public JButton getYellow()
+	{
+		return yellow;
+	}
+	
+	public JButton getRed()
+	{
+		return red;
+	}
+	
+	public JButton getBlue()
+	{
+		return blue;
+	}
+	
+	public JButton getGreen()
+	{
+		return green;
+	}
+	
+	public JButton getMagenta()
+	{
+		return magenta;
+	}
 }
