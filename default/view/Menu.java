db198bf39096c3a60a3a5b757affdfd189843c4c
yoliner <yoline@172.17.68.224>
2017-04-03 11:21:32 +0200

    Classe Menu (Panels et Boutons)

--- default/view/Menu.java
+++ default/view/Menu.java
@@ -1,5 +1,36 @@
 package view;
 
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeCellRenderer;
+import javax.swing.event.*;
+import javax.swing.*;
+import java.awt.*;
+
 public class Menu {
 
+	/**
+	 * Attributs
+	 */
+	private JPanel utilisateur, menu;
+	private JButton enseignant, eleve; 
+	
+	/**
+	 * Constructeur
+	 */
+	public Menu(){
+		
+		//Panel utilisateur qui correspond à la page permettant le choix entre le compte Enseignant et le compte Eleve
+		utilisateur = new JPanel();
+		
+		//Initialisation des boutons du menu utilisateur
+		enseignant = new JButton("Enseignant");
+		eleve = new JButton("Eleve");
+		
+		//Panel menu contiendra le menu suivant le type de compte (enseignant ou eleve)
+		menu = new JPanel();
+		
+		
+		//relier les classes Titre, Affichage et ActionMenu
+		
+	}
 }
