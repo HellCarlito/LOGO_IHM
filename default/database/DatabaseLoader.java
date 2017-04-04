bb7c688a3ebe1d31b51692a12cfe399d8c880534
carlito <charles.tholliez@openmailbox.org>
2017-04-04 16:40:41 +0200

    commentaire

--- default/database/DatabaseLoader.java
+++ default/database/DatabaseLoader.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 /*
  * Cette classe permet de charger les donnees presentes dans la base
@@ -12,7 +14,7 @@ import java.sql.Statement;
 
 public class DatabaseLoader {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws SQLException {
 		loadDatabase();
 	}
 
@@ -21,13 +23,21 @@ public class DatabaseLoader {
 	public static void loadDatabase() throws SQLException {
 		DatabaseConnection connectionDB = new DatabaseConnection();
 		String sqlRequest = ("select * from eleve");
+		HashMap<String, String> hashmapEleves; // Clé / Valeur 
 
 		try (Connection connection = connectionDB.connect();
 				Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery(sqlRequest)) {
 			while (rs.next()) {
+				hashmapEleves = new HashMap<String, String>();
 				// lit les resultats
-				System.out.println("prenom = " + rs.getString("prenom_eleve") + " - nom = " + rs.getString("nom_eleve"));
+				//System.out.println("prenom = " + rs.getString("prenom_eleve") + " - nom = " + rs.getString("nom_eleve"));
+				hashmapEleves.put("nom", rs.getString("nom_eleve"));
+				hashmapEleves.put("prenom", rs.getString("prenom_eleve"));
+				//System.out.println(hashmapEleves);
+				for (Entry<String, String> entry : hashmapEleves.entrySet()) {
+					System.out.println(entry.getKey() + " " + entry.getValue());
+				}
 			}
 
 		}
