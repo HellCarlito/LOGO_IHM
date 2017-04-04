4f6564a8b266c7064af432c4c74e7a5ed8803cda
Chloé <Chlo@172.17.68.100>
2017-04-04 16:26:29 +0200

    ajouter une ligne correspondant à une action dans l'interface code action

--- default/view/Code.java
+++ default/view/Code.java
@@ -1,7 +1,8 @@
 package view;
 import java.awt.GridLayout;
-
 import javax.swing.*;
+import model.*;
+import model.Action;
 
 public class Code extends JPanel
 {	
@@ -9,5 +10,15 @@ public class Code extends JPanel
 	public Code()
 	{
 		this.setLayout(new GridLayout(0,1));
+	}
+	
+	public void ajouterAction(Action uneAction)
+	{
+		JLabel ligneActionIcone=new JLabel(uneAction.getIcone());
+		JLabel ligneActionTexte=new JLabel(uneAction.getCodeAction());
+		JPanel ligneAction=new JPanel(new GridLayout(1,0));
+		ligneAction.add(ligneActionIcone);
+		ligneAction.add(ligneActionTexte);
+		this.add(ligneAction);
 	}
 }
