728395c34d38d75712f086f22f2848624398a457
Chloé <Chlo@172.17.68.100>
2017-04-04 15:02:03 +0200

    boutons ok

--- default/view/ZoneBouton.java
+++ default/view/ZoneBouton.java
@@ -1,5 +1,37 @@
 package view;
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+import model.*;
+import view.*;
 
-public class ZoneBouton {
-
+public class ZoneBouton extends JPanel
+{
+	private JPanel haut;
+	
+	public ZoneBouton(TortueG maTortue)
+	{
+		haut=new JPanel(new BorderLayout());
+		haut.add(new BoutonDeplacement(),BorderLayout.CENTER);
+		if (maTortue instanceof TortueCouleur)
+		{
+			haut.add(new BoutonCouleur(),BorderLayout.EAST);
+		}
+		else if (maTortue instanceof TortueRapide)
+		{
+			haut.add(new BoutonRapide(),BorderLayout.EAST);
+		}
+		this.setLayout(new GridLayout (0,1));
+		this.add(haut);
+		this.add(new BoutonGestion());
+	}
+	
+	public static void main(String[] args)
+	{
+		TortueG uneTortue= new TortueG();
+        JFrame test = new JFrame();
+        test.setContentPane(new ZoneBouton(uneTortue));
+        test.setVisible(true);
+        test.pack();
+    }
 }
