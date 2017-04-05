753ebab071e04bb4e9e626b80a1f2bfcbdd7b1d6
agath <agath@172.17.72.45>
2017-04-05 16:56:33 +0200

    prout

--- default/view/MenuUtilisateur.java
+++ default/view/MenuUtilisateur.java
@@ -21,17 +21,13 @@ public class MenuUtilisateur extends JPanel{
 	public MenuUtilisateur()
 	{
 		//Initialisation des Panels et Boutons pour la page utilisateur
-				utilisateur = new JPanel(new GridLayout(2,1));
-				boutonsUtilisateur = new JPanel(new GridLayout(1,2));
+				boutonsUtilisateur = new JPanel(new GridLayout(2,1));
 				enseignant = new JButton("Enseignant");
 				eleve = new JButton("Eleve");
-				enteteMenu = new Titre("Utilisateur");
-				utilisateur.add(enteteMenu);
 				boutonsUtilisateur.add(enseignant);
 				boutonsUtilisateur.add(eleve);
-				utilisateur.add(boutonsUtilisateur);
 				
-				this.add(utilisateur);
+				this.add(boutonsUtilisateur);
 	}
 	
 	public JButton getEnseignant(){
