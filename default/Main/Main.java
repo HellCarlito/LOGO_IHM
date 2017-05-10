3ab6668156b65bfb4f979a2432919f11a98739a4
HellCarlito <charles.tholliez@openmailbox.org>
2017-05-10 14:12:52 +0200

    Update main with database creation

--- default/Main/Main.java
+++ default/Main/Main.java
@@ -2,15 +2,20 @@ package Main;
 
 import javax.swing.JFrame;
 
+import database.DatabaseCreation;
 import view.Menu;
 
-public class Main
-{
-	public static void main(String[] args)
-	{
+public class Main {
+
+	public static void main(String[] args) {
+		// Appelle de la classe DatabaseCreation crée une connexion
+		// avec création d'un nouvelle base de données
+		DatabaseCreation dbCreate = new DatabaseCreation();
+		// TODO Auto-generated method stub
 		 JFrame test = new JFrame();
-	     test.setContentPane(new Menu());
-	     test.setVisible(true);
-	     test.pack();
+	        test.setContentPane(new Menu());
+	        test.setVisible(true);
+	        test.pack();
 	}
+
 }
