f5c4ec79a742376f659e29e221355d843f69f4a2
Chloé <Chlo@172.17.67.227>
2017-05-10 13:53:34 +0200

    urgzolh

---
+++ default/view/ActionEleve.java
@@ -0,0 +1,52 @@
+package view;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class ActionEleve extends JPanel
+{
+	// Attributs
+	private JPanel boutonsMenuEleve,affichage;
+	private JButton faireExercice, visualiserTentative, refaireTentative, visualisationResultats;
+	
+	public ActionEleve() 
+	{
+		//Initialisation des boutons du menu de l'interface Eleve
+		faireExercice = new JButton ("Faire un exercice");
+		visualiserTentative = new JButton("Visualiser une tentative");
+		refaireTentative = new JButton("Refaire une tentative");
+		visualisationResultats = new JButton("Visualiser mes résultats");
+	
+		//Création du Panel de boutons du menu Eleve
+		boutonsMenuEleve = new JPanel(new GridLayout(0,1));
+		boutonsMenuEleve.add(faireExercice);
+		boutonsMenuEleve.add(visualiserTentative);
+		boutonsMenuEleve.add(refaireTentative);
+		boutonsMenuEleve.add(visualisationResultats);
+		
+		//Creation panel affichage resultats
+		affichage = new JPanel(new GridLayout(0,1));
+		
+		this.setLayout(new GridLayout(1,0));
+		this.add(boutonsMenuEleve);
+		this.add(affichage);
+	}
+	
+	public JButton getFaireExo(){
+		return faireExercice;
+	}
+	
+	public JButton getVisualiserTentative(){
+		return visualiserTentative;
+	}
+	
+	public JButton getRefaireTentative(){
+		return refaireTentative;
+	}
+	
+	public JButton getVisualiserResultat(){
+		return visualisationResultats;
+	}
+}
---
+++ default/view/ActionEnseignant.java
@@ -0,0 +1,74 @@
+package view;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeCellRenderer;
+import javax.swing.event.*;
+import javax.swing.*;
+import java.awt.*;
+  
+public class ActionEnseignant extends JPanel
+{
+
+	//Attributs
+	private JPanel boutonsMenuEnseignant, affichage;
+	private JButton creerExercice, modifierExercice, explorerTravailEleve, evaluerTravailEleve, visualiserClasse;
+	
+	
+	/**
+	 * Classe principale de la classe ActionEnseignant 
+	 * @return un Panel
+	 */
+	public ActionEnseignant() 
+	{	
+		//Initialisation des boutons du menu de l'interface Enseignant
+		creerExercice = new JButton("Créer un exercice");
+		modifierExercice = new JButton("Modifier un exercice");
+		explorerTravailEleve = new JButton("Explorer le travail d'un élève");
+		evaluerTravailEleve = new JButton("Evaluer le travail des élèves");
+		visualiserClasse = new JButton("Visualiser une classe");
+		
+		
+		//Creation du Panel de boutons du menu Enseignant
+		boutonsMenuEnseignant = new JPanel(new GridLayout(0,1));
+		boutonsMenuEnseignant.add(creerExercice);
+		boutonsMenuEnseignant.add(modifierExercice);
+		boutonsMenuEnseignant.add(explorerTravailEleve);
+		boutonsMenuEnseignant.add(evaluerTravailEleve);
+		boutonsMenuEnseignant.add(visualiserClasse);
+		
+		//Creation panel affichage resultats
+		affichage = new JPanel(new GridLayout(0,1));
+		
+		this.setLayout(new GridLayout(1,0));
+		this.add(boutonsMenuEnseignant);
+		this.add(affichage);
+		
+	}
+	
+		
+
+	
+	public JButton getCreerExo(){
+		return creerExercice;
+	}
+	
+	public JButton getModifExo(){
+		return modifierExercice;
+	}
+	
+	public JButton getExplorerTravail(){
+		return explorerTravailEleve;
+	}
+	
+	public JButton getEvaluerEleve(){
+		return evaluerTravailEleve;
+	}
+	
+	public JButton getVisualiserClasse(){
+		return visualiserClasse;
+	}	
+	
+	public JPanel getActionMenu(){
+		return this;
+	}
+}
